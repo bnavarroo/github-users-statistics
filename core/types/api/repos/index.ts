e186c3b1d5081028a5e969da9af9ef4f1215ca98
Bruno Navarro <62071446+bnavarroo@users.noreply.github.com>
2021-11-19 19:03:31 -0300

    Ajuste na url do repositório do usuário.

---
+++ core/types/api/repos/index.ts
@@ -0,0 +1,6 @@
+/* eslint-disable camelcase */
+export type UserReposType = {
+  name: string;
+  url: string;
+  html_url: string;
+};
---
+++ pages/user/[username]/components/repos/index.tsx
@@ -0,0 +1,39 @@
+import {
+  Wrapper,
+  Title,
+  RepoList,
+  RepoBox,
+  RepoTitle,
+  RepoLink,
+} from './styles';
+
+type ReposProps = {
+  repos: Array<{
+    name: string;
+    url: string;
+    // eslint-disable-next-line camelcase
+    html_url: string;
+  }>;
+};
+
+const Repos: React.FC<ReposProps> = ({ repos }) => (
+  <Wrapper>
+    <Title>Repositórios Públicos</Title>
+    <RepoList>
+      {repos.length > 0 ? (
+        repos.map((repo) => (
+          <RepoBox key={repo.name}>
+            <RepoTitle>{repo.name}</RepoTitle>
+            <RepoLink href={repo.html_url} target="_blank">
+              {repo.html_url}
+            </RepoLink>
+          </RepoBox>
+        ))
+      ) : (
+        <div>Nenhum repositório encontrado</div>
+      )}
+    </RepoList>
+  </Wrapper>
+);
+
+export default Repos;
